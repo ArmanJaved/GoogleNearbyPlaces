c574b24280e4d9c64f1af7b7e108a781494de62c
Arman Javed <arman.javed@brainplow.com>
2017-10-06 19:28:50 +0500

    2nd commit.

--- app/src/main/java/com/mytrendin/googlenearbyplaces/GetNearbyBanksData.java
+++ app/src/main/java/com/mytrendin/googlenearbyplaces/GetNearbyBanksData.java
@@ -1,16 +1,23 @@
 package com.mytrendin.googlenearbyplaces;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.AsyncTask;
 import android.util.Log;
 
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.gson.Gson;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+
+import static android.content.Context.MODE_PRIVATE;
 
 public class GetNearbyBanksData extends AsyncTask<Object, String, String> {
 
@@ -18,6 +25,18 @@ public class GetNearbyBanksData extends AsyncTask<Object, String, String> {
     GoogleMap mMap;
     String url;
 
+    Context context;
+    ArrayList<String> list = new ArrayList<String>();
+    public static final String MY_PREFS_NAME = "settings";
+
+    public static final String LatlngNearBY = "restlatlng";
+
+
+
+    GetNearbyBanksData (MapsActivity mapsActivity)
+    {
+        this.context = mapsActivity;
+    }
     @Override
     protected String doInBackground(Object... params) {
         try {
@@ -44,7 +63,10 @@ public class GetNearbyBanksData extends AsyncTask<Object, String, String> {
     }
 
     private void ShowNearbyPlaces(List<HashMap<String, String>> nearbyPlacesList) {
+
+
         for (int i = 0; i < nearbyPlacesList.size(); i++) {
+
             Log.d("onPostExecute","Entered into showing locations");
             MarkerOptions markerOptions = new MarkerOptions();
             HashMap<String, String> googlePlace = nearbyPlacesList.get(i);
@@ -57,9 +79,36 @@ public class GetNearbyBanksData extends AsyncTask<Object, String, String> {
             markerOptions.title(placeName + " : " + vicinity);
             mMap.addMarker(markerOptions);
             markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED));
-            //move map camera
-            mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
-            mMap.animateCamera(CameraUpdateFactory.zoomTo(11));
+            CameraUpdate center= CameraUpdateFactory.newLatLng(latLng);
+            CameraUpdate zoom=CameraUpdateFactory.zoomTo(15);
+            mMap.moveCamera(center);
+            mMap.animateCamera(zoom);
+            list.add(placeName);
+
+            if (i==0)
+            {
+                SharedPreferences.Editor editor = context.getSharedPreferences(LatlngNearBY, MODE_PRIVATE).edit();
+                String lalng= lat + "-" + lng;
+                editor.putString("Latlnfg", lalng);
+                editor.apply();
+            }
+
+
         }
+
+
+        SharedPreferences.Editor editor = context.getSharedPreferences(MY_PREFS_NAME, MODE_PRIVATE).edit();
+        Gson gson = new Gson();
+        List<String> textList = new ArrayList<String>();
+        textList.addAll(list);
+        String jsonText = gson.toJson(textList);
+        editor.putString("key", jsonText);
+        editor.commit();
+
+
     }
+
+
+
+
 }
--- app/src/main/java/com/mytrendin/googlenearbyplaces/MapsActivity.java
+++ app/src/main/java/com/mytrendin/googlenearbyplaces/MapsActivity.java
@@ -1,15 +1,30 @@
 package com.mytrendin.googlenearbyplaces;
 
 import android.Manifest;
+import android.animation.ObjectAnimator;
+import android.animation.PropertyValuesHolder;
+import android.animation.ValueAnimator;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.content.ContextCompat;
+import android.util.DisplayMetrics;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Switch;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
@@ -18,14 +33,17 @@ import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.LocationListener;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.gson.Gson;
+
+import java.util.ArrayList;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback,
         GoogleApiClient.ConnectionCallbacks,
@@ -40,13 +58,57 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     Marker mCurrLocationMarker;
     LocationRequest mLocationRequest;
     private GoogleMap mMap;
-    private int PROXIMITY_RADIUS = 30;
+    private double PROXIMITY_RADIUS = 45;
+
+    public static final String DontWantTAG = "Dontshow";
+    public static final String LatlngNearBY = "restlatlng";
+
+
+    private ObjectAnimator waveOneAnimator;
+    private ObjectAnimator waveTwoAnimator;
+    private ObjectAnimator waveThreeAnimator;
+    private ObjectAnimator waveFourAnimator;
+    private ObjectAnimator waveFiveAnimator;
+
+
+    private TextView hangoutTvOne;
+    private TextView hangoutTvTwo;
+    private TextView hangoutTvThree;
+    private TextView hangoutTvfour;
+    private TextView hangoutTvfive;
+
+    private int screenWidth;
+    Switch simpleSwitch;
+    public static final String TotalAmount = "totalamount";
+
+
+    Button RestList;
+
+    ArrayList<String> list = new ArrayList<String>();
 
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+
+
+
+
+        simpleSwitch = (Switch) findViewById(R.id.switch1);
+        simpleSwitch.setEnabled(false);
+
+
+
+        RestList = (Button)findViewById(R.id.restbutton);
+
+        RestList.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                startActivity(new Intent( MapsActivity.this, DataRestDisplay.class));
+            }
+        });
 
 
         if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
@@ -60,10 +122,45 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         }
 
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+
+        //********* hangout ************
+        hangoutTvOne = (TextView) findViewById(R.id.hangoutTvOne);
+        hangoutTvTwo = (TextView) findViewById(R.id.hangoutTvTwo);
+        hangoutTvThree = (TextView) findViewById(R.id.hangoutTvThree);
+        hangoutTvfour = (TextView) findViewById(R.id.hangoutTvFour);
+        hangoutTvfive = (TextView) findViewById(R.id.hangoutTvFive);
+
+
+
+        hangoutTvOne.setVisibility(View.GONE);
+        hangoutTvTwo.setVisibility(View.GONE);
+        hangoutTvThree.setVisibility(View.GONE);
+        hangoutTvfour.setVisibility(View.GONE);
+        hangoutTvfive.setVisibility(View.GONE);
+
+
+
+        DisplayMetrics displaymetrics = new DisplayMetrics();
+        this.getWindowManager().getDefaultDisplay().getMetrics(displaymetrics);
+        screenWidth = displaymetrics.widthPixels;
+
+
+        hangoutTvOne.setVisibility(View.VISIBLE);
+        hangoutTvTwo.setVisibility(View.VISIBLE);
+        hangoutTvThree.setVisibility(View.VISIBLE);
+        hangoutTvfour.setVisibility(View.VISIBLE);
+        hangoutTvfive.setVisibility(View.VISIBLE);
+
+
+        waveAnimation();
+
+
     }
 
     private boolean CheckGooglePlayServices() {
@@ -96,22 +193,19 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             mMap.setMyLocationEnabled(true);
         }
 
-        Button btnBanks = (Button) findViewById(R.id.btnBanks);
-        btnBanks.setOnClickListener(new View.OnClickListener() {
-            String search = "food";
-            @Override
-            public void onClick(View v) {
-                mMap.clear();
-                String url = getUrl(latitude, longitude, search);
-                Object[] DataTransfer = new Object[2];
-                DataTransfer[0] = mMap;
-                DataTransfer[1] = url;
-                GetNearbyBanksData getNearbyBanksData = new GetNearbyBanksData();
-                getNearbyBanksData.execute(DataTransfer);
-                Toast.makeText(MapsActivity.this, "These are your Nearby Banks! ", Toast.LENGTH_LONG).show();
-            }
-        });
     }
+
+    public void nearbyrest()
+    {
+        String search = "restaurant";
+        String url = getUrl(latitude, longitude, search);
+        Object[] DataTransfer = new Object[2];
+        DataTransfer[0] = mMap;
+        DataTransfer[1] = url;
+        GetNearbyBanksData getNearbyBanksData = new GetNearbyBanksData(MapsActivity.this);
+        getNearbyBanksData.execute(DataTransfer);
+    }
+
 
     protected synchronized void buildGoogleApiClient() {
         mGoogleApiClient = new GoogleApiClient.Builder(this)
@@ -162,20 +256,149 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         //Place current location marker
         latitude = location.getLatitude();
         longitude = location.getLongitude();
+
+
         LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
         MarkerOptions markerOptions = new MarkerOptions();
         markerOptions.position(latLng);
-        markerOptions.title("You are Here!");
-        markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE));
-        mCurrLocationMarker = mMap.addMarker(markerOptions);
+//        markerOptions.title("You are Here!");
+//        markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE));
+//        mCurrLocationMarker = mMap.addMarker(markerOptions);
 
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
-        mMap.animateCamera(CameraUpdateFactory.zoomTo(11));
-        Toast.makeText(MapsActivity.this, "Your Current Location", Toast.LENGTH_LONG).show();
+//        mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+//        mMap.animateCamera(CameraUpdateFactory.zoomTo(11));
+        CameraUpdate center= CameraUpdateFactory.newLatLng(new LatLng(latitude, longitude));
+        CameraUpdate zoom=CameraUpdateFactory.zoomTo(15);
+
+        mMap.moveCamera(center);
+        mMap.animateCamera(zoom);
+      //  Toast.makeText(MapsActivity.this, "Your Current Location", Toast.LENGTH_LONG).show();
 
         if (mGoogleApiClient != null) {
             LocationServices.FusedLocationApi.removeLocationUpdates(mGoogleApiClient, this);
         }
+
+
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                //Do something after 6s
+                SharedPreferences prefs = getSharedPreferences(DontWantTAG, MODE_PRIVATE);
+                String name = prefs.getString("Alertdisplay", "No name defined");//"No name defined" is the default value.
+
+                SharedPreferences prefs1 = getSharedPreferences(LatlngNearBY, MODE_PRIVATE);
+                String lalng_name1 = prefs1.getString("Latlnfg", "No name defined");//"No name defined" is the default value.
+
+
+                if (name.equals("DONT"))
+                {
+                    String[] latlngArray = lalng_name1.split("-");
+
+                    String latnear1= latlngArray[0];
+                    String lngnear1 = latlngArray[1];
+
+                    Location locationA = new Location("point A");
+                    locationA.setLatitude(latitude);
+                    locationA.setLongitude(longitude);
+
+                    Location locationB = new Location("point B");
+                    locationB.setLatitude(Double.parseDouble(latnear1));
+                    locationB.setLongitude(Double.parseDouble(lngnear1));
+
+                    float distance = locationA.distanceTo(locationB);
+                    int distchange = (int) distance;
+
+                    if (distchange>50)
+                    {
+
+                        hangoutTvOne.setVisibility(View.GONE);
+                        hangoutTvTwo.setVisibility(View.GONE);
+                        hangoutTvThree.setVisibility(View.GONE);
+                        hangoutTvfour.setVisibility(View.GONE);
+                        hangoutTvfive.setVisibility(View.GONE);
+                        nearbyrest();
+                        handler.postDelayed(new Runnable() {
+                            @Override
+                            public void run() {
+                                //Do something after 8s
+                                listdisplay();
+                            }
+                        }, 6000);
+                    }
+                    else return;
+
+                }
+                else
+                {
+
+                    hangoutTvOne.setVisibility(View.GONE);
+                    hangoutTvTwo.setVisibility(View.GONE);
+                    hangoutTvThree.setVisibility(View.GONE);
+                    hangoutTvfour.setVisibility(View.GONE);
+                    hangoutTvfive.setVisibility(View.GONE);
+                    nearbyrest();
+                    handler.postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            //Do something after 8s
+                            listdisplay();
+                        }
+                    }, 3000);
+                }
+
+            }
+        }, 6000);
+
+
+
+    }
+
+
+    public void listdisplay()
+    {
+
+        SharedPreferences prefs = getApplicationContext().getSharedPreferences("settings", Context.MODE_PRIVATE);
+        Gson gson = new Gson();
+        String jsonText = prefs.getString("key", null);
+        String[] text = gson.fromJson(jsonText, String[].class);
+
+
+        AlertDialog.Builder builderSingle = new AlertDialog.Builder(MapsActivity.this);
+        builderSingle.setIcon(R.drawable.fai_logo_gradients);
+        builderSingle.setTitle("Have you been to this place today ? ");
+
+        final ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(MapsActivity.this, android.R.layout.select_dialog_singlechoice,text);
+
+        builderSingle.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+
+        builderSingle.setPositiveButton("Don't show", new DialogInterface.OnClickListener() {
+
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+                SharedPreferences.Editor editor = getSharedPreferences(DontWantTAG, MODE_PRIVATE).edit();
+                editor.putString("Alertdisplay", "DONT");
+                editor.apply();
+
+                simpleSwitch.setChecked(false);
+
+            }
+        });
+
+        builderSingle.setAdapter(arrayAdapter, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                String strName = arrayAdapter.getItem(which);
+                avc(strName);
+            }
+        });
+        builderSingle.show();
     }
 
     @Override
@@ -203,9 +426,10 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         }
     }
 
+
+
     @Override
-    public void onRequestPermissionsResult(int requestCode,
-                                           String permissions[], int[] grantResults) {
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
         switch (requestCode) {
             case MY_PERMISSIONS_REQUEST_LOCATION: {
                 if (grantResults.length > 0
@@ -226,6 +450,96 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 return;
             }
         }
+    }
+
+    public void avc(final String res_name) {
+        LayoutInflater inflater = getLayoutInflater();
+        View alertLayout = inflater.inflate(R.layout.dialogue, null);
+        final EditText amount_txt = (EditText) alertLayout.findViewById(R.id.et_username);
+
+
+        android.support.v7.app.AlertDialog.Builder alert = new android.support.v7.app.AlertDialog.Builder(this);
+        alert.setTitle("How much did you spend in "+ res_name+ " ? ");
+        // this is set the view from XML inside AlertDialog
+        alert.setView(alertLayout);
+        // disallow cancel of AlertDialog on click of back button and outside touch
+        alert.setCancelable(false);
+        alert.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(getBaseContext(), "Cancel clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        alert.setPositiveButton("Add", new DialogInterface.OnClickListener() {
+
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                String amount = amount_txt.getText().toString();
+               // String pass = etPassword.getText().toString();
+
+                Toast.makeText(getBaseContext(), "Amount: " + amount  , Toast.LENGTH_SHORT).show();
+                AddData obj = new AddData(MapsActivity.this);
+                obj.addartist (res_name, amount);
+
+            }
+        });
+
+        android.support.v7.app.AlertDialog dialog = alert.create();
+        dialog.show();
+
+
+    }
+
+
+
+    public void waveAnimation() {
+        PropertyValuesHolder tvOne_Y = PropertyValuesHolder.ofFloat(hangoutTvOne.TRANSLATION_Y, -40.0f);
+        PropertyValuesHolder tvOne_X = PropertyValuesHolder.ofFloat(hangoutTvOne.TRANSLATION_X, 0);
+        waveOneAnimator = ObjectAnimator.ofPropertyValuesHolder(hangoutTvOne, tvOne_X, tvOne_Y);
+        waveOneAnimator.setRepeatCount(-1);
+        waveOneAnimator.setRepeatMode(ValueAnimator.REVERSE);
+        waveOneAnimator.setDuration(400);
+        waveOneAnimator.start();
+
+        PropertyValuesHolder tvTwo_Y = PropertyValuesHolder.ofFloat(hangoutTvTwo.TRANSLATION_Y, -40.0f);
+        PropertyValuesHolder tvTwo_X = PropertyValuesHolder.ofFloat(hangoutTvTwo.TRANSLATION_X, 0);
+        waveTwoAnimator = ObjectAnimator.ofPropertyValuesHolder(hangoutTvTwo, tvTwo_X, tvTwo_Y);
+        waveTwoAnimator.setRepeatCount(-1);
+        waveTwoAnimator.setRepeatMode(ValueAnimator.REVERSE);
+        waveTwoAnimator.setDuration(400);
+        waveTwoAnimator.setStartDelay(300);
+        waveTwoAnimator.start();
+
+        PropertyValuesHolder tvThree_Y = PropertyValuesHolder.ofFloat(hangoutTvThree.TRANSLATION_Y, -40.0f);
+        PropertyValuesHolder tvThree_X = PropertyValuesHolder.ofFloat(hangoutTvThree.TRANSLATION_X, 0);
+        waveThreeAnimator = ObjectAnimator.ofPropertyValuesHolder(hangoutTvThree, tvThree_X, tvThree_Y);
+        waveThreeAnimator.setRepeatCount(-1);
+        waveThreeAnimator.setRepeatMode(ValueAnimator.REVERSE);
+        waveThreeAnimator.setDuration(400);
+        waveThreeAnimator.setStartDelay(400);
+        waveThreeAnimator.start();
+
+        PropertyValuesHolder tvFour_Y = PropertyValuesHolder.ofFloat(hangoutTvfour.TRANSLATION_Y, -40.0f);
+        PropertyValuesHolder tvFour_X = PropertyValuesHolder.ofFloat(hangoutTvfour.TRANSLATION_X, 0);
+        waveFourAnimator = ObjectAnimator.ofPropertyValuesHolder(hangoutTvfour, tvFour_Y, tvFour_X);
+        waveFourAnimator.setRepeatCount(-1);
+        waveFourAnimator.setRepeatMode(ValueAnimator.REVERSE);
+        waveFourAnimator.setDuration(400);
+        waveFourAnimator.setStartDelay(500);
+        waveFourAnimator.start();
+
+        PropertyValuesHolder tvFive_Y = PropertyValuesHolder.ofFloat(hangoutTvfive.TRANSLATION_Y, -40.0f);
+        PropertyValuesHolder tvFive_X = PropertyValuesHolder.ofFloat(hangoutTvfive.TRANSLATION_X, 0);
+        waveFiveAnimator = ObjectAnimator.ofPropertyValuesHolder(hangoutTvfive, tvFive_Y, tvFive_X);
+        waveFiveAnimator.setRepeatCount(-1);
+        waveFiveAnimator.setRepeatMode(ValueAnimator.REVERSE);
+        waveFiveAnimator.setDuration(400);
+        waveFiveAnimator.setStartDelay(600);
+        waveFiveAnimator.start();
+
+
     }
 
 }
